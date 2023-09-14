bc5e05716c1cd70ac727c198ee83103d76e51606
manishpatil2003 <pmonu5214@gmail.com>
2023-09-14 12:00:16 +0530

    Commit-Text-editor

---
+++ src/components/TextEditor.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { Editor } from "react-draft-wysiwyg";
+import { EditorState, convertToRaw } from "draft-js";
+
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import draftToHtml from "draftjs-to-html";
+
+export default class TextEditor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editorState: EditorState.createEmpty(),
+    };
+  }
+
+  onEditorStateChange = (editorState) => {
+    this.setState((prevState) => ({
+      editorState,
+      htmlContent: draftToHtml(convertToRaw(editorState.getCurrentContent())),
+    }));
+  };
+
+  render() {
+    const { editorState} = this.state;
+    return (
+      <div className="border border-5" > 
+        <Editor
+          editorState={editorState}
+          toolbarClassName="toolbarClassName"
+          wrapperClassName="wrapperClassName"
+          editorClassName="editorClassName"
+          onEditorStateChange={this.onEditorStateChange}
+        />
+        
+      </div>
+    );
+  }
+}
